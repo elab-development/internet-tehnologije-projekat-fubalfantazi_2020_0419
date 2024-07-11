c1dd551152c8df3d7dbdf85002d74e1433322d38
Sava Pavlovic <ea.pavlovic.37@gmail.com>
2024-07-11 20:25:07 +0200

    api fajl

---
+++ fantasy-api/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GameStatController;
+use App\Http\Controllers\LeagueController;
+use App\Http\Controllers\LeagueMemberController;
+use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\RoundController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\UserTeamController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
