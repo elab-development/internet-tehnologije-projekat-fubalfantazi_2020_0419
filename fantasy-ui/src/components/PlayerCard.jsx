6241370d8be9caf8ca58ebeda04976658b67c812
Minjole64 <minjovic30@gmail.com>
2024-07-13 14:15:50 +0200

    player card komponenta

---
+++ fantasy-ui/src/components/PlayerCard.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+import {Card} from "react-bootstrap";
+import axiosInstance from "../axiosInstance";
+
+const PlayerCard = props => {
+    const {player, position, teams} = props;
+    const [team, setTeam] = useState(null);
+
+
+    return (
+        <>
+            <Card className="mt-3">
+                <Card.Body className="text-center">
+                    <Card.Title><h3>({position}) {player.player_name}</h3></Card.Title>
+                    <Card.Text>
+                        <span>Team: {teams.find(team => team.id === player.team_id).team_name}</span>
+                        <br />
+                        <span>Value: {player.player_price}M </span>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+    );
+};
+
+PlayerCard.propTypes = {
+    player: PropTypes.object.isRequired,
+    position: PropTypes.number.isRequired,
+    teams: PropTypes.array.isRequired
+};
+
+export default PlayerCard;
